27b18c852be1930e331af7aac6430afb54c6d307
huGgW <whjoon0225@naver.com>
2022-12-22 21:13:52 +0900

    🎉 Empty project initialized

---
+++ src/test/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wafflestudio.team2.jisik2n
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Jisik2nApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
