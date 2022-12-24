b125d6deb2e0947374c1cef37a2bfca2751ec078
huGgW <whjoon0225@naver.com>
2022-12-24 19:16:22 +0900

    Restore to proper linted code

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplication.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplication.kt
@@ -4,26 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 @SpringBootApplication
 class Jisik2nApplication
-fun main(args: Array<String>) {                      runApplication<Jisik2nApplication>(*args);
+fun main(args: Array<String>) {
+    runApplication<Jisik2nApplication>(*args)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
