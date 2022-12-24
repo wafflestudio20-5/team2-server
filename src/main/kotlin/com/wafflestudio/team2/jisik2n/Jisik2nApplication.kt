4af7fe7cc26fc7834761eeacab77e9929b74a090
huGgW <whjoon0225@naver.com>
2022-12-24 19:09:13 +0900

    Not properly linted codes for testing

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplication.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplication.kt
@@ -2,10 +2,28 @@ package com.wafflestudio.team2.jisik2n
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
 @SpringBootApplication
 class Jisik2nApplication
-
-fun main(args: Array<String>) {
-	runApplication<Jisik2nApplication>(*args)
+fun main(args: Array<String>) {                      runApplication<Jisik2nApplication>(*args);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplicationTests.kt
+++ src/test/kotlin/com/wafflestudio/team2/jisik2n/Jisik2nApplicationTests.kt
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class Jisik2nApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+@Test
+fun contextLoads() {                            }
 }
