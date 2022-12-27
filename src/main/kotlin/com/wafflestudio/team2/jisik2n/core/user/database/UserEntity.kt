e449c18334bbf8dfab8f4421b36692a946aa369b
jiwon79 <19-079@ksa.hs.kr>
2022-12-27 15:59:29 +0900

    :sparkles: UserEntity 추가

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/database/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/database/UserEntity.kt
@@ -1,4 +1,13 @@
 package com.wafflestudio.team2.jisik2n.core.user.database
 
-class UserEntity {
+import com.wafflestudio.team2.jisik2n.common.BaseTimeEntity
+import javax.persistence.*
+
+@Entity
+class UserEntity(
+    var uid: String,
+    var username: String,
+    var password: String,
+) : BaseTimeEntity() {
+
 }
