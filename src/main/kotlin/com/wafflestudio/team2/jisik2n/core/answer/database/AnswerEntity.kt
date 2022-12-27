0df291f5aa0682b7fb49ce2e3bd7d60138ffe6f0
jiwon79 <19-079@ksa.hs.kr>
2022-12-27 16:01:22 +0900

    :sparkles: Answer Entity 추가

---
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/answer/database/AnswerEntity.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.team2.jisik2n.core.answer.database
+
+import com.wafflestudio.team2.jisik2n.common.BaseTimeEntity
+import javax.persistence.Entity
+
+@Entity
+class AnswerEntity(
+    var content: String,
+    var like: Long,
+): BaseTimeEntity() {
+}
