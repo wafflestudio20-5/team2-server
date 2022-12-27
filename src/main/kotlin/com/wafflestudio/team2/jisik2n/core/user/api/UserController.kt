7264971f18768c7f57d3ffa7129e2b268b3bf054
jiwon79 <19-079@ksa.hs.kr>
2022-12-27 16:10:31 +0900

    :sparkles: Hello world api 추가

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/api/UserController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/api/UserController.kt
@@ -1,4 +1,12 @@
 package com.wafflestudio.team2.jisik2n.core.user.api
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class UserController {
+    @GetMapping("/users")
+    fun getUsers(): String {
+        return "Hello World"
+    }
 }
