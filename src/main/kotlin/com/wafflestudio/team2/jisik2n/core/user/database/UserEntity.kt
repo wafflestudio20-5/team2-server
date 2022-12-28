47e5e06d77a5849b9fb875923a36b4c559de9c4e
huGgW <whjoon0225@naver.com>
2022-12-28 20:54:24 +0900

    Modified UserEntity

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/database/UserEntity.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/database/UserEntity.kt
@@ -1,11 +1,22 @@
 package com.wafflestudio.team2.jisik2n.core.user.database
 
 import com.wafflestudio.team2.jisik2n.common.BaseTimeEntity
+import java.time.LocalDateTime
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
 class UserEntity(
-    var uid: String,
+    @Column(unique = true, nullable = true)
+    val uid: String? = null,
+    @Column(unique = true, nullable = true)
+    val snsId: String? = null,
+    @Column(unique = true)
     var username: String,
     var password: String,
+    @Column(columnDefinition = "datetime(6) default '1999-01-01'")
+    var lastLogin: LocalDateTime,
+    var isMale: Boolean,
+    @Column(nullable = true)
+    var profileImage: String?,
 ) : BaseTimeEntity()
