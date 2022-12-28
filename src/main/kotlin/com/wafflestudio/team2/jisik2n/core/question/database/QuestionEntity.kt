dae58955b34c604664a39f66e406d0998659c94c
huGgW <whjoon0225@naver.com>
2022-12-28 22:00:37 +0900

    Modified QuestionEntity

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/database/QuestionEntity.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/database/QuestionEntity.kt
@@ -1,11 +1,40 @@
 package com.wafflestudio.team2.jisik2n.core.question.database
 
 import com.wafflestudio.team2.jisik2n.common.BaseTimeEntity
+import com.wafflestudio.team2.jisik2n.core.answer.database.AnswerEntity
+import com.wafflestudio.team2.jisik2n.core.photo.database.PhotoEntity
+import com.wafflestudio.team2.jisik2n.core.user.database.UserEntity
+import java.time.LocalDateTime
+import javax.persistence.CascadeType
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
 
 @Entity
 class QuestionEntity(
-    var title: String,
+    @Column(nullable = true)
+    var title: String? = null,
+
     var content: String,
-    var like: Long,
+
+    @OneToMany(cascade = [CascadeType.ALL])
+    @JoinColumn
+    val photos: MutableSet<PhotoEntity> = mutableSetOf(),
+
+    @OneToMany(mappedBy = "question", cascade = [CascadeType.ALL])
+    val answers: MutableSet<AnswerEntity> = mutableSetOf(),
+
+    var close: Boolean = false,
+
+    @Column(columnDefinition = "datetime(6)", nullable = true)
+    var closedAt: LocalDateTime? = null,
+
+    @ManyToOne
+    @JoinColumn
+    val user: UserEntity,
+
+    // TODO: Add likes
+
 ) : BaseTimeEntity()
