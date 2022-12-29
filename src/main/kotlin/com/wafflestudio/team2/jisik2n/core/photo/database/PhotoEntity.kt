e8ad9b6b3cb97edf4065eac741905c6ded8fd7ba
jiwon79 <19-079@ksa.hs.kr>
2022-12-29 21:18:21 +0900

    :bug: PhotoEntity id 값을 지정 안 하도록 변경

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/photo/database/PhotoEntity.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/photo/database/PhotoEntity.kt
@@ -3,10 +3,11 @@ package com.wafflestudio.team2.jisik2n.core.photo.database
 import javax.persistence.*
 
 @Entity
-class PhotoEntity(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
-
+public class PhotoEntity(
     @Column
     val path: String,
-)
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+}
