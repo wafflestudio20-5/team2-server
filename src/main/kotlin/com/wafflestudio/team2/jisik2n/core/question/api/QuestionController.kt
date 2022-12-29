9c92f7a0db37fa65064bdce64edce54dac6231a2
jiwon79 <19-079@ksa.hs.kr>
2022-12-29 19:40:21 +0900

    :sparkles: Question Controller 검색 api 추가

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/api/QuestionController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/api/QuestionController.kt
@@ -1,3 +1,17 @@
 package com.wafflestudio.team2.jisik2n.core.question.api
 
-class QuestionController
+import com.wafflestudio.team2.jisik2n.core.question.database.QuestionEntity
+import com.wafflestudio.team2.jisik2n.core.question.service.QuestionService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class QuestionController(
+    private val questionService: QuestionService,
+) {
+    @GetMapping("/api/question/search")
+    fun searchQuestion(): MutableList<QuestionEntity> {
+        return questionService.searchQuestion()
+    }
+}
