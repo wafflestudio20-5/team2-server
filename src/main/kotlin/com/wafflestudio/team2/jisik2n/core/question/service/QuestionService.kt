0998fcba6e779059305e6244037431c1731601a6
jiwon79 <19-079@ksa.hs.kr>
2022-12-29 19:39:47 +0900

    :sparkles: Question Service 검색함수 Template 추가

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/service/QuestionService.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/service/QuestionService.kt
@@ -1,3 +1,14 @@
 package com.wafflestudio.team2.jisik2n.core.question.service
 
-class QuestionService
+import com.wafflestudio.team2.jisik2n.core.question.database.QuestionEntity
+import com.wafflestudio.team2.jisik2n.core.question.database.QuestionRepository
+import org.springframework.stereotype.Service
+
+@Service
+class QuestionService(
+    private val questionRepository: QuestionRepository
+) {
+    fun searchQuestion(): MutableList<QuestionEntity> {
+        return questionRepository.findAll()
+    }
+}
