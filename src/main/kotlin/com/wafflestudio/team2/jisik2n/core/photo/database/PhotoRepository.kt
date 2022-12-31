4818badc5a326035697e4b6f0693c77f0b31a19e
jiwon79 <19-079@ksa.hs.kr>
2022-12-31 14:46:50 +0900

    :hammer: lint 테스트

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/photo/database/PhotoRepository.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/photo/database/PhotoRepository.kt
@@ -2,4 +2,4 @@ package com.wafflestudio.team2.jisik2n.core.photo.database
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PhotoRepository: JpaRepository<PhotoEntity, Long>
+interface PhotoRepository : JpaRepository<PhotoEntity, Long>
