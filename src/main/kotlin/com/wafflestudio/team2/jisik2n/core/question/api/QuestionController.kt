e9cb37b184c2198abba283aeb0defdf968be41b7
huGgW <whjoon0225@naver.com>
2022-12-31 20:45:16 +0900

    Remove initial creation of test user

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/api/QuestionController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/api/QuestionController.kt
@@ -3,13 +3,12 @@ package com.wafflestudio.team2.jisik2n.core.question.api
 import com.wafflestudio.team2.jisik2n.core.question.api.request.CreateQuestionRequest
 import com.wafflestudio.team2.jisik2n.core.question.database.QuestionEntity
 import com.wafflestudio.team2.jisik2n.core.question.service.QuestionService
-import com.wafflestudio.team2.jisik2n.core.user.database.UserEntity
 import com.wafflestudio.team2.jisik2n.core.user.database.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 import javax.validation.Valid
 
 @RestController
@@ -17,17 +16,6 @@ class QuestionController(
     private val questionService: QuestionService,
     private val userRepository: UserRepository,
 ) {
-    // TODO: Add authentication
-    final val user: UserEntity = UserEntity(
-        username = "test",
-        password = "test",
-        isMale = true,
-        profileImage = null,
-        lastLogin = LocalDateTime.now(),
-    )
-    init {
-        userRepository.save(user)
-    }
 
     @GetMapping("/api/question/search")
     fun searchQuestion(): MutableList<QuestionEntity> {
@@ -38,6 +26,7 @@ class QuestionController(
     fun createQuestion(
         @Valid @RequestBody request: CreateQuestionRequest,
     ): QuestionEntity {
+        val user = userRepository.findByIdOrNull(1)!! // TODO: Authentication
         return questionService.createQuestion(request, user)
     }
 }
