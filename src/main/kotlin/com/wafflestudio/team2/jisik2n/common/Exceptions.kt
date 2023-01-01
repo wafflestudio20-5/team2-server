03064f06c5ccf2c3ff1951c0eca1d9fa4e015739
huGgW <whjoon0225@naver.com>
2023-01-01 10:28:50 +0900

    Added FORBIDEN 403 Exception

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/common/Exceptions.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/common/Exceptions.kt
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus
 open class Jisik2nException(msg: String, val status: HttpStatus) : RuntimeException(msg)
 
 class Jisik2n400(msg: String) : Jisik2nException(msg, HttpStatus.BAD_REQUEST)
-
 class Jisik2n401(msg: String) : Jisik2nException(msg, HttpStatus.UNAUTHORIZED)
 class Jisik2n404(msg: String) : Jisik2nException(msg, HttpStatus.NOT_FOUND)
+class Jisik2n403(msg: String) : Jisik2nException(msg, HttpStatus.UNAUTHORIZED)
