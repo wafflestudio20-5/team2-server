9fd3577df3b8cd43cb21ea672ffa067ded32be62
huGgW <whjoon0225@naver.com>
2023-01-08 21:42:22 +0900

    Added testing api for image upload & remove

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/photo/api/PhotoController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/photo/api/PhotoController.kt
@@ -1,10 +1,9 @@
 package com.wafflestudio.team2.jisik2n.core.photo.api
 
 import com.wafflestudio.team2.jisik2n.external.s3.service.S3Service
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
@@ -12,8 +11,14 @@ import org.springframework.web.multipart.MultipartFile
 class PhotoController(
     private val s3Service: S3Service,
 ) {
-    @PostMapping(consumes = ["multipart/form-data"])
+    @PostMapping("/test", consumes = ["multipart/form-data"])
     fun uploadPhoto(@RequestParam image: MultipartFile): String {
         return s3Service.upload(image)
+    }
+
+    @DeleteMapping("/test")
+    fun deletePhoto(@RequestParam(name = "url") url: String): ResponseEntity<Nothing> {
+        s3Service.delete(url)
+        return ResponseEntity<Nothing>(HttpStatus.OK)
     }
 }
