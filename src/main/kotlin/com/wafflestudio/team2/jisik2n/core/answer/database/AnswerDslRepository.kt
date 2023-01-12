44bfe820d7cfad9c8c80bd887258baf4cbd35c36
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2023-01-12 09:00:59 +0900

    getMyAnswers 생성 수정

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/answer/database/AnswerDslRepository.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/answer/database/AnswerDslRepository.kt
@@ -1,3 +1,18 @@
 package com.wafflestudio.team2.jisik2n.core.answer.database
 
-class AnswerDslRepository
+import com.querydsl.core.types.Projections
+import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.team2.jisik2n.core.user.dto.Answers
+import org.springframework.stereotype.Component
+
+@Component
+class AnswerDslRepository(
+    private val queryFactory: JPAQueryFactory
+) {
+
+    fun getMyAnswers(username: String): List<Answers> {
+        val answerEntity: QAnswerEntity = QAnswerEntity.answerEntity
+        return queryFactory.select(Projections.constructor(Answers::class.java, answerEntity.id, answerEntity.content))
+            .from(answerEntity).where(answerEntity.user.username.eq(username)).fetch()
+    }
+}
