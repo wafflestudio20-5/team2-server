e94766bf963aedef7cfc9f11d4c08e370333898f
huGgW <whjoon0225@naver.com>
2023-01-15 15:49:00 +0900

    Update DTO

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/dto/UpdateQuestionRequest.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/dto/UpdateQuestionRequest.kt
@@ -1,7 +1,9 @@
 package com.wafflestudio.team2.jisik2n.core.question.dto
 
+import com.wafflestudio.team2.jisik2n.core.photo.dto.PhotoRequest
+
 data class UpdateQuestionRequest(
     val title: String?,
     val content: String?,
-    val photos: List<String> = emptyList(),
+    val photos: List<PhotoRequest> = emptyList(),
 )
