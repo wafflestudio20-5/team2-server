1f6cd105ff6144083155ab450af2b971306c0314
huGgW <whjoon0225@naver.com>
2023-01-17 21:16:25 +0900

    Added SearchResponse DTO

---
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/dto/SearchResponse.kt
@@ -0,0 +1,12 @@
+package com.wafflestudio.team2.jisik2n.core.question.dto
+
+import com.querydsl.core.annotations.QueryProjection
+
+data class SearchResponse @QueryProjection constructor(
+    val questionId: Long,
+    val title: String,
+    val content: String,
+    val answerContent: String,
+    var answerCount: Long,
+    var questionLikeCount: Long,
+)
