c0346152293db99a071226dbd48c75a8179fab04
huGgW <whjoon0225@naver.com>
2023-01-17 23:46:37 +0900

    Update controller

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/api/QuestionController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/api/QuestionController.kt
@@ -6,6 +6,7 @@ import com.wafflestudio.team2.jisik2n.common.SEARCH_ORDER
 import com.wafflestudio.team2.jisik2n.common.UserContext
 import com.wafflestudio.team2.jisik2n.core.question.dto.CreateQuestionRequest
 import com.wafflestudio.team2.jisik2n.core.question.dto.QuestionDto
+import com.wafflestudio.team2.jisik2n.core.question.dto.SearchResponse
 import com.wafflestudio.team2.jisik2n.core.question.dto.UpdateQuestionRequest
 import com.wafflestudio.team2.jisik2n.core.question.service.QuestionService
 import com.wafflestudio.team2.jisik2n.core.user.database.UserEntity
@@ -23,20 +24,19 @@ class QuestionController(
     fun searchQuestion(
         @RequestParam(required = false, defaultValue = "date") order: String,
         @RequestParam(required = false, defaultValue = "null") isClosed: String,
-        @RequestParam(required = false) amount: Long = 20,
-        @RequestParam(required = false) pageNum: Long = 0,
+        @RequestParam(required = false, defaultValue = "20") amount: Long,
+        @RequestParam(required = false, defaultValue = "0") pageNum: Long,
         @RequestParam(required = false, defaultValue = "") keyword: String,
-    ) = try {
-        val orderEnum = SEARCH_ORDER.valueOf(order)
+    ): List<SearchResponse> {
+        val orderEnum = SEARCH_ORDER.values().find { it.value == order }
+            ?: throw Jisik2n400("order 의 값이 잘못되었습니다.")
         val isClosedBoolean = when (isClosed) {
             "true" -> true
             "false" -> false
             "null" -> null
             else -> throw Jisik2n400("isClosed 의 값이 잘못되었습니다.")
         }
-        questionService.searchQuestion(orderEnum, isClosedBoolean, keyword, amount, pageNum)
-    } catch (e: IllegalArgumentException) {
-        throw Jisik2n400("order 의 값이 잘못되었습니다.")
+        return questionService.searchQuestion(orderEnum, isClosedBoolean, keyword, amount, pageNum)
     }
 
     @Authenticated
