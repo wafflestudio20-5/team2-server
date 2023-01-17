eb9c6016e935b1c2c6aa862f3defd68234c2665d
huGgW <whjoon0225@naver.com>
2023-01-17 21:16:10 +0900

    Added query for searching

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/database/QuestionRepository.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/database/QuestionRepository.kt
@@ -2,6 +2,11 @@ package com.wafflestudio.team2.jisik2n.core.question.database
 
 import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.team2.jisik2n.common.SEARCH_ORDER
+import com.wafflestudio.team2.jisik2n.core.answer.database.QAnswerEntity.answerEntity
+import com.wafflestudio.team2.jisik2n.core.question.database.QQuestionEntity.questionEntity
+import com.wafflestudio.team2.jisik2n.core.question.dto.QSearchResponse
+import com.wafflestudio.team2.jisik2n.core.question.dto.SearchResponse
 import com.wafflestudio.team2.jisik2n.core.user.dto.QuestionsOfMyAllProfile
 import com.wafflestudio.team2.jisik2n.core.user.dto.QuestionsOfMyQuestions
 import org.springframework.data.jpa.repository.JpaRepository
@@ -11,8 +16,14 @@ interface QuestionRepository : JpaRepository<QuestionEntity, Long>, CustomQuesti
 
 interface CustomQuestionRepository {
     fun getQuestionsOfMyQuestions(username: String): List<QuestionsOfMyQuestions>
-
     fun getQuestionsOfMyAllProfile(username: String): List<QuestionsOfMyAllProfile>
+    fun searchAndOrderPagination(
+        order: SEARCH_ORDER,
+        isClosed: Boolean? = null,
+        keyword: String,
+        amount: Long,
+        pageNum: Long
+    ): MutableList<SearchResponse>
 }
 
 @Component
@@ -49,5 +60,40 @@ class QuestionRepositoryImpl(
             )
         )
             .from(questionEntity).where(questionEntity.user.username.eq(username)).fetch()
+    }
+
+    override fun searchAndOrderPagination(order: SEARCH_ORDER, isClosed: Boolean?, keyword: String, amount: Long, pageNum: Long): MutableList<SearchResponse> {
+        val searchResponses = queryFactory.select(
+            QSearchResponse(
+                questionEntity.id,
+                questionEntity.title,
+                questionEntity.content,
+                answerEntity.content,
+                questionEntity.answerCount,
+                questionEntity.likeCount,
+            )
+        ).from(questionEntity)
+            .join(questionEntity.answers, answerEntity).fetchJoin()
+            .where(
+                questionEntity.title.contains(keyword) // Search for keywords
+                    .or((questionEntity.content.contains(keyword)))
+                    .or((answerEntity.content.contains(keyword))),
+                when (isClosed) { // Filter with close
+                    true -> questionEntity.close.eq(true)
+                    false -> questionEntity.close.eq(false)
+                    null -> null
+                }
+            )
+            .orderBy(
+                when (order) { // Order by date or like
+                    SEARCH_ORDER.DATE -> questionEntity.createdAt.asc()
+                    SEARCH_ORDER.LIKE -> questionEntity.likeCount.asc()
+                }
+            )
+            .offset(pageNum) // pagination
+            .limit(amount)
+            .fetch()
+
+        return searchResponses
     }
 }
