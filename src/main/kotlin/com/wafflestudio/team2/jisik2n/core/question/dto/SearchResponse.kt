d54c9701b8ba02cfcd99df8cec461839c81a0a3e
huGgW <whjoon0225@naver.com>
2023-01-17 23:48:46 +0900

    Add nullability to answerContent

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/dto/SearchResponse.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/dto/SearchResponse.kt
@@ -6,7 +6,7 @@ data class SearchResponse @QueryProjection constructor(
     val questionId: Long,
     val title: String,
     val content: String,
-    val answerContent: String,
+    val answerContent: String?,
     var answerCount: Long,
     var questionLikeCount: Long,
 )
