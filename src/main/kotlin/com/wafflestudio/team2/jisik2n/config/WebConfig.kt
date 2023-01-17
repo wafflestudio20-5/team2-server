4aa050aeb53b280eb23e8dcf4077252ea51dc146
Jo Seonggyu <blueland1998@naver.com>
2023-01-17 20:53:43 +0900

    Feature/auth3.1 인터셉터 Authentication(유저 확인) -> 필요하면 Authorization(권한) (#42)
    * test
    
    * 메서드 정리
    
    * :sparkles: 인터셉터 Authenticated -> Authorization 생성
    
    * :yellow_heart: user 수정에서 닉네임도 수정 가능
    
    * getKakaoToken(백 전용 토큰 획득 메서드) return값을 readline으로 수정

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/config/WebConfig.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/config/WebConfig.kt
@@ -4,8 +4,6 @@ import com.wafflestudio.team2.jisik2n.common.Authenticated
 import com.wafflestudio.team2.jisik2n.common.Jisik2n400
 import com.wafflestudio.team2.jisik2n.common.Jisik2n401
 import com.wafflestudio.team2.jisik2n.common.UserContext
-import com.wafflestudio.team2.jisik2n.core.user.database.BlacklistTokenRepository
-import com.wafflestudio.team2.jisik2n.core.user.database.TokenRepository
 import com.wafflestudio.team2.jisik2n.core.user.database.UserEntity
 import com.wafflestudio.team2.jisik2n.core.user.database.UserRepository
 import com.wafflestudio.team2.jisik2n.core.user.service.AuthTokenService
@@ -26,11 +24,13 @@ import javax.servlet.http.HttpServletResponse
 
 @Configuration
 class WebConfig(
-    private val authInterceptor: AuthInterceptor,
+    private val authInterceptor1: AuthInterceptor1,
+    private val authInterceptor2: AuthInterceptor2,
     private val authArgumentResolver: AuthArgumentResolver,
 ) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(authInterceptor)
+        registry.addInterceptor(authInterceptor1).order(1)
+        registry.addInterceptor(authInterceptor2).order(2)
     }
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
@@ -58,69 +58,43 @@ class AuthArgumentResolver : HandlerMethodArgumentResolver {
 }
 
 @Configuration
-class AuthInterceptor(
+class AuthInterceptor1(
     private val authTokenService: AuthTokenService,
-    private val tokenRepository: TokenRepository,
     private val userRepository: UserRepository,
-    private val blacklistTokenRepository: BlacklistTokenRepository,
 ) : HandlerInterceptor {
 
-//    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-//        val handlerCasted = (handler as? HandlerMethod) ?: return true
-//        if (handlerCasted.hasMethodAnnotation(Authenticated::class.java)) {
-//            val accessToken = request.getHeader("Authorization") ?: throw Jisik2n401("access Token 획득 실패")
-//            val refreshToken = request.getHeader("RefreshToken") ?: throw Jisik2n401("refresh Token 획득 실패")
-//
-//            val prefixRemovedAccessToken = accessToken.replace("Bearer ", "").trim { it <= ' ' }
-//            val prefixRemovedRefreshToken = refreshToken.replace("Bearer ", "").trim { it <= ' ' }
-//            if (blacklistTokenRepository.findByAccessToken(prefixRemovedAccessToken) != null) {
-//                throw Jisik2n401("token이 만료되었습니다")
-//            }
-//            if (authTokenService.verifyToken(prefixRemovedRefreshToken) != true) {
-//                throw Jisik2n401("refresh token이 적절하지 않습니다.")
-//            }
-//
-//            if (authTokenService.verifyToken(prefixRemovedAccessToken) == true) { // access token 정상적 작동
-//                val userId = authTokenService.getCurrentUserId(prefixRemovedAccessToken)
-//                val userEntity = userRepository.findByIdOrNull(userId)
-//                request.setAttribute("userEntity", userEntity)
-//            } else { // access token이 만료되었거나, 존재하지도 않거나
-//
-//                val tokenEntity = tokenRepository.findByAccessToken(prefixRemovedAccessToken)
-//                if (tokenEntity == null) { // access token이 존재하지 않음
-//
-//                    throw Jisik2n401("access token이 적절하지 않습니다.")
-//                } else { // access token 만료됨
-//
-//                    if (prefixRemovedRefreshToken == tokenEntity.refreshToken) {
-//                        val newAccessToken = authTokenService.generateAccessTokenByUid(tokenEntity.keyUid)
-//                        tokenEntity.accessToken = newAccessToken
-//                        tokenRepository.save(tokenEntity)
-//                        val userId = authTokenService.getCurrentUserId(newAccessToken)
-//                        val userEntity = userRepository.findByIdOrNull(userId)
-//                        request.setAttribute("userEntity", userEntity)
-//                    } else {
-//                        throw Jisik2n401("refresh token이 적절하지 않습니다.")
-//                    }
-//                }
-//            }
-//        }
-//
-//        return super.preHandle(request, response, handler)
-//    }
-
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        val handlerCasted = (handler as? HandlerMethod) ?: return true
-        if (handlerCasted.hasMethodAnnotation(Authenticated::class.java)) {
-            val accessToken = request.getHeader("Authorization") ?: throw Jisik2n400("access Token 획득 실패")
+        // Authentication: 유저인지 확인한다 => accessToken 없으면 안주고, 틀렸으면 401, 맞으면 주고
+        val accessToken = request.getHeader("Authorization")
+        if (accessToken == null) {
+            throw Jisik2n400("token을 제공하지 않았습니다")
+        } else if (accessToken == "") {
+            request.setAttribute("userEntity", null)
+        } else {
             val prefixRemovedAccessToken = accessToken.replace("Bearer ", "").trim { it <= ' ' }
-
             if (authTokenService.verifyToken(prefixRemovedAccessToken) == true) {
                 val userId = authTokenService.getCurrentUserId(prefixRemovedAccessToken)
                 val userEntity = userRepository.findByIdOrNull(userId)
                 request.setAttribute("userEntity", userEntity)
             } else {
                 throw Jisik2n401("access token이 적절하지 않습니다.")
+            }
+        }
+
+        return super.preHandle(request, response, handler)
+    }
+}
+
+@Configuration
+class AuthInterceptor2 : HandlerInterceptor {
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        // Authorization: 권한을 확인한다 => 유저정보 없으면 401, 있으면 컨트롤러 실행 가능
+        val handlerCasted = (handler as? HandlerMethod) ?: return true
+
+        if (handlerCasted.hasMethodAnnotation(Authenticated::class.java)) {
+            if (request.getAttribute("userEntity") == null) {
+                throw Jisik2n401("로그인을 해야 합니다")
             }
         }
 
