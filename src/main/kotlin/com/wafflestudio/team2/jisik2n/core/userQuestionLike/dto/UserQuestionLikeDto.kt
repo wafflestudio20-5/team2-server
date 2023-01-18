f01c8f0d45fb78704629220508b3bf939fec7806
jiwon79 <19-079@ksa.hs.kr>
2023-01-18 23:52:12 +0900

    :sparkles: UserQuestionLike 에서 QuestionDto 를 반환하도록 변경

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/userQuestionLike/dto/UserQuestionLikeDto.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/userQuestionLike/dto/UserQuestionLikeDto.kt
@@ -1,18 +1,20 @@
 package com.wafflestudio.team2.jisik2n.core.userQuestionLike.dto
 
+import com.wafflestudio.team2.jisik2n.core.question.dto.QuestionDto
 import com.wafflestudio.team2.jisik2n.core.userQuestionLike.database.UserQuestionLikeEntity
+import com.wafflestudio.team2.jisik2n.external.s3.service.S3Service
 
 data class UserQuestionLikeDto(
     val id: Long,
     val userId: Long,
-    val questionId: Long,
+    val question: QuestionDto,
 ) {
     companion object {
-        fun of(entity: UserQuestionLikeEntity): UserQuestionLikeDto {
+        fun of(entity: UserQuestionLikeEntity, s3Service: S3Service): UserQuestionLikeDto {
             return UserQuestionLikeDto(
                 id = entity.id,
                 userId = entity.user.id,
-                questionId = entity.question.id,
+                question = QuestionDto.of(entity.question, s3Service),
             )
         }
     }
