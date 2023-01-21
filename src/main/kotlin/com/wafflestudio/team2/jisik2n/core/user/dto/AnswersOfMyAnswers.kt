26a745ad3f4247f7fde91f47d343b8f2dd7325ab
Jo Seonggyu <blueland1998@naver.com>
2023-01-21 15:04:23 +0900

    Feature/auth3.4 💛 나의질문/나의대답/좋아요누른질문 response 형태 변경 (#59)
    * :yellow_heart: 나의시리즈 response 바꿈
    
    * 답변 questionId->id로 변경

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/dto/AnswersOfMyAnswers.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/dto/AnswersOfMyAnswers.kt
@@ -1,6 +1,9 @@
 package com.wafflestudio.team2.jisik2n.core.user.dto
 
+import java.time.LocalDateTime
+
 data class AnswersOfMyAnswers(
     val id: Long,
-    val content: String
+    val title: String,
+    val createdAt: LocalDateTime
 )
