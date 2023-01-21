ab7a4bd563c53d87cca2de0f60fa357366a8ef50
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2023-01-21 15:03:31 +0900

    답변 questionId->id로 변경

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/dto/AnswersOfMyAnswers.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/dto/AnswersOfMyAnswers.kt
@@ -3,7 +3,7 @@ package com.wafflestudio.team2.jisik2n.core.user.dto
 import java.time.LocalDateTime
 
 data class AnswersOfMyAnswers(
-    val questionId: Long,
+    val id: Long,
     val title: String,
     val createdAt: LocalDateTime
 )
