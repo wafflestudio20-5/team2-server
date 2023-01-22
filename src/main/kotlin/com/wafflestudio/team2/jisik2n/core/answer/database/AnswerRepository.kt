475cc7f79e42eea76cf6ddb0315ab717e5c8e6c0
huGgW <whjoon0225@naver.com>
2023-01-23 00:03:53 +0900

    Optimized to 4 queries per get

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/answer/database/AnswerRepository.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/answer/database/AnswerRepository.kt
@@ -2,10 +2,18 @@ package com.wafflestudio.team2.jisik2n.core.answer.database
 
 import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.wafflestudio.team2.jisik2n.core.answer.database.QAnswerEntity.answerEntity
+import com.wafflestudio.team2.jisik2n.core.answer.dto.AnswerResponse
+import com.wafflestudio.team2.jisik2n.core.photo.database.QPhotoEntity.photoEntity
 import com.wafflestudio.team2.jisik2n.core.question.database.QQuestionEntity
+import com.wafflestudio.team2.jisik2n.core.question.database.QQuestionEntity.questionEntity
+import com.wafflestudio.team2.jisik2n.core.user.database.QUserEntity.userEntity
 import com.wafflestudio.team2.jisik2n.core.user.database.UserEntity
 import com.wafflestudio.team2.jisik2n.core.user.dto.AnswersOfMyAllProfile
 import com.wafflestudio.team2.jisik2n.core.user.dto.AnswersOfMyAnswers
+import com.wafflestudio.team2.jisik2n.core.userAnswerInteraction.database.QUserAnswerInteractionEntity.userAnswerInteractionEntity
+import com.wafflestudio.team2.jisik2n.core.userAnswerInteraction.dto.UserAnswerInteractionCountResponse
+import com.wafflestudio.team2.jisik2n.external.s3.service.S3Service
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
@@ -18,6 +26,11 @@ interface AnswerRepository : JpaRepository<AnswerEntity, Long>, CustomAnswerRepo
 }
 
 interface CustomAnswerRepository {
+    fun getAnswerOfQuestionId(
+        questionId: Long,
+        loginUser: UserEntity?
+    ): List<AnswerResponse>
+
     fun getAnswersOfMyAnswers(username: String): List<AnswersOfMyAnswers>
 
     fun getAnswersOfMyAllProfile(username: String): List<AnswersOfMyAllProfile>
@@ -25,8 +38,96 @@ interface CustomAnswerRepository {
 
 @Component
 class CustomAnswerRepositoryImpl(
-    private val queryFactory: JPAQueryFactory
+    private val queryFactory: JPAQueryFactory,
+    private val s3Service: S3Service,
 ) : CustomAnswerRepository {
+    override fun getAnswerOfQuestionId(
+        questionId: Long,
+        loginUser: UserEntity?
+    ): List<AnswerResponse> {
+//        println("===0")
+        // Find answers
+        val answers = queryFactory.select(answerEntity)
+            .from(answerEntity)
+            // join question
+            .join(answerEntity.question, questionEntity).fetchJoin()
+            // join user
+            .join(answerEntity.user, userEntity).fetchJoin()
+            // join userAnswerInteraction
+            .leftJoin(answerEntity.userAnswerInteractions, userAnswerInteractionEntity).fetchJoin()
+            // join photo
+            .leftJoin(answerEntity.photos, photoEntity).fetchJoin()
+            .where(questionEntity.id.eq(questionId))
+            .orderBy(
+                answerEntity.selected.desc(),
+                answerEntity.createdAt.asc()
+            )
+            .distinct()
+            .fetch()
+//        println("===1")
+
+        // Join userAnswerInteraction to answer
+        queryFactory.selectFrom(userAnswerInteractionEntity)
+            .join(userAnswerInteractionEntity.user, userEntity).fetchJoin()
+            .join(userAnswerInteractionEntity.answer, answerEntity).fetchJoin()
+            .where(answerEntity.`in`(answers))
+            .fetch()
+//        println("===2")
+
+        // Find user`s most recent answer date
+        val userRecentAnswerDateTuple = queryFactory
+            .select(
+                userEntity.id,
+                answerEntity.createdAt.min()
+            )
+            .from(answerEntity)
+            .join(answerEntity.user, userEntity)
+            .groupBy(
+                answerEntity.user.id,
+                answerEntity.selected,
+                answerEntity.createdAt,
+            )
+            .where(userEntity.`in`(answers.map { it.user }))
+            .orderBy(
+                answerEntity.selected.desc(),
+                answerEntity.createdAt.asc(),
+            )
+            .fetch()
+//        println("===3")
+
+        return answers.map {
+            AnswerResponse(
+                id = it.id,
+                content = it.content,
+                photos = it.photos.sortedBy {
+                    it.photosOrder
+                }.map {
+                    s3Service.getUrlFromFilename(it.path)
+                },
+                createdAt = it.createdAt!!,
+                selected = it.selected,
+                selectedAt = it.selectedAt,
+                interactionCount = let { _ ->
+                    val agreeCnt = it.userAnswerInteractions.count { it.isAgree }
+                    val disagreeCnt = it.userAnswerInteractions.size - agreeCnt
+                    UserAnswerInteractionCountResponse(
+                        agree = agreeCnt,
+                        disagree = disagreeCnt,
+                    )
+                },
+                userId = it.user.id,
+                username = it.user.username,
+                profileImagePath = it.user.profileImage
+                    ?.let { path -> s3Service.getUrlFromFilename(path) },
+                userRecentAnswerDate = userRecentAnswerDateTuple.find { t -> it.user.id == t[userEntity.id] }!!
+                    .let { t -> t[answerEntity.createdAt.min()]!! },
+                userIsAgreed = it.userAnswerInteractions
+                    .find { userAnswerInteraction -> userAnswerInteraction.user == loginUser }
+                    ?.run { isAgree }
+            )
+        }
+    }
+
     override fun getAnswersOfMyAnswers(username: String): List<AnswersOfMyAnswers> {
         val answerEntity: QAnswerEntity = QAnswerEntity.answerEntity
         val questionEntity: QQuestionEntity = QQuestionEntity.questionEntity
