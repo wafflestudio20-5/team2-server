535727b1d9d1729671bb8d312151b71f0ee69600
Jo Seonggyu <blueland1998@naver.com>
2023-01-23 22:11:23 +0900

    :sparkles: 회원 탈퇴 생성, refreshToken 만료되면 로그인시 재발급 (#62)
    * :sparkles: 회원 탈퇴 생성, refreshToken 만료되면 로그인시 재발급
    
    * :yeallow_heart: 대소문자 로그인 구분

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/api/UserController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/api/UserController.kt
@@ -98,8 +98,12 @@ class UserController(
         return userService.putAccount(userEntity, userRequest)
     }
 
-    fun deleteAccount() {
-        // 탈퇴할 때, 질문이랑 대답 조회해서 거기에 있는 사용자 null로 바꿔버리기
+    @Authenticated
+    @DeleteMapping("deleteAccount")
+    fun deleteAccount(
+        @UserContext userEntity: UserEntity
+    ): String {
+        return userService.deleteAccount(userEntity)
     }
 
     @PostMapping("regenerateToken")
