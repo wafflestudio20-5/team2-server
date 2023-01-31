4e27b3b629ac8afa763ad715c51ecc28ce2e35c8
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2023-01-31 22:03:04 +0900

    :yellow_heat: 카카오로그인 isActive, lastLogin 저장

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/api/UserController.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/user/api/UserController.kt
@@ -41,7 +41,6 @@ class UserController(
 
     @GetMapping("kakaoLogin")
     fun kakaoLogin(@RequestParam(value = "accessToken", required = false) accessToken: String): LoginResponse {
-        println("test")
         return userService.kakaoLogin(accessToken)
     }
 
