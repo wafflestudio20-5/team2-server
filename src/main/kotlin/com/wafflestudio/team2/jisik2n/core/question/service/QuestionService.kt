c77f690444fa6d10f91e2815e960d5389a6ec5c1
huGgW <whjoon0225@naver.com>
2023-02-02 00:23:42 +0900

    Remove comment

--- src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/service/QuestionService.kt
+++ src/main/kotlin/com/wafflestudio/team2/jisik2n/core/question/service/QuestionService.kt
@@ -67,13 +67,6 @@ class QuestionServiceImpl(
         return QuestionDto.of(newQuestion, s3Service)
     }
 
-//    @Transactional
-//    override fun getQuestion(questionId: Long): QuestionDto {
-//        val question: Optional<QuestionEntity> = questionRepository.findById(questionId)
-//        if (question.isEmpty) throw Jisik2n400("존재하지 않는 질문 번호 입니다.(questionId: $questionId)")
-//
-//        return QuestionDto.of(question.get(), s3Service)
-//    }
     override fun getQuestion(questionId: Long) =
         questionRepository.findQuestionDtoByIdOrNull(questionId)
             ?: throw Jisik2n400("존재하지 않는 질문 번호 입니다.(questionId: $questionId)")
